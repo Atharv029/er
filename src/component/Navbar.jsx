8865e6c14ae85589353f4e43e8e9b5011f8d4fa8
Atharv029 <160618505+Atharv029@users.noreply.github.com>
2024-06-27 20:59:38 +0530

    Update Navbar.jsx

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
+// import React from 'react'
 
-const Navbar = () => {
-  return (
+// const Navbar = () => {
+//   return (
 
-    <div className="navbar z-0">
-     <a href="#home"><li>Home</li>  </a>
-      <a href="#ser">Service</a>
-     <a href="https://online.publuu.com/556425/1250886" target='blank'> <li>Resume</li></a> 
-     <a href="#Project"><li>Portfolio</li>  </a> 
-      <a href="#Abouts"><li>About</li> </a>
-      <div className="contact font-bold">
-        <a href="https://wa.me/9892467712" target='blank'>Contact Us</a>
-      </div>
+//     <div className="navbar z-0">
+//      <a href="#home"><li>Home</li>  </a>
+//       <a href="#ser">Service</a>
+//      <a href="https://online.publuu.com/556425/1250886" target='blank'> <li>Resume</li></a> 
+//      <a href="#Project"><li>Portfolio</li>  </a> 
+//       <a href="#Abouts"><li>About</li> </a>
+//       <div className="contact font-bold">
+//         <a href="https://wa.me/9892467712" target='blank'>Contact Us</a>
+//       </div>
       
-    </div>
+//     </div>
 
-  )
-}
+//   )
+// }
 
-export default Navbar
+// export default Navbar
